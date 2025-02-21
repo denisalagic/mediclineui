32a4328e8ecfbc43ded56c29630f52798ea8a830
Denis Alagic <denisalagic@windowslive.com>
2025-02-21 18:29:27 +0100

    colors

---
+++ lib/constants/mcolors.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class MColors {
+  static const grey = Color(0xFF4A4A4A);
+  static const lightGrey = Color(0xFFD6D6D6);
+  static const primaryGreen = Color(0xFF82c685);
+  static const darkGreen = Color(0xFF5fb663);
+  static const lightGreen = Color(0xFFA5D6A7);
+  static const lightYellow = Color(0xFFFFF59D);
+  static const lightOrange = Color(0xFFFFCC80);
+  static const lightRed = Color(0xFFEF9A9A);
+  static const darkRed = Color(0xFFe55858);
+
+  static const lightBlue = Color(0xFFBBDEFB);
+  static Color listBackground = Colors.grey.shade100;
+
+  static Color hintText = Color(0xFFA7A7A7);
+}
+
+
--- lib/mediclineui.dart
+++ lib/mediclineui.dart
@@ -1,2 +1,4 @@
 export 'package:mediclineui/mdropdown/mdropdown.dart';
+export 'package:mediclineui/constants/mcolors.dart';
+export 'package:mediclineui/constants/mtext_styles.dart';
 
