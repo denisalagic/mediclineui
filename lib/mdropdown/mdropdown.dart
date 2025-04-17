7c27e0a37ed6906a2bda86ad988176cf25ca1ef5
dalagic <denisalagichr@gmail.com>
2025-04-17 17:57:32 +0200

    mtextfield

--- lib/mdropdown/mdropdown.dart
+++ lib/mdropdown/mdropdown.dart
@@ -614,6 +614,7 @@ class _MDropdownState<T> extends State<MDropdown<T>> {
               //errorStyle: decoration?.errorStyle ?? _defaultErrorStyle,
               errorText: formFieldState.errorText,
               border: InputBorder.none,
+              contentPadding: const EdgeInsets.all(8.0),
               //contentPadding: EdgeInsets.zero,
             ),
             child: _OverlayBuilder(
