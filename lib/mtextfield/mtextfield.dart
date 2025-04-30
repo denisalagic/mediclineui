b16ad5adb7c0707230f1dc696b3ed9513223bdf2
dalagic <denisalagichr@gmail.com>
2025-04-30 20:03:38 +0200

    mlabel

--- lib/mtextfield/mtextfield.dart
+++ lib/mtextfield/mtextfield.dart
@@ -29,54 +29,57 @@ class MTextField extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return TextFormField(
-      validator: (val) {
-        return validator != null ? validator!(val) : null;
-      },
-      autovalidateMode: AutovalidateMode.onUserInteraction,
-      controller: controller,
-      decoration: InputDecoration(
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(8.0),
-          borderSide: BorderSide(width: 0, style: BorderStyle.none),
-        ),
-        hoverColor: Colors.white,
-        focusColor: Colors.white,
-        filled: true,
-        fillColor: Colors.white,
-        errorBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(8),
-          borderSide: BorderSide(
-            color: Colors.red,
-            width: 1.5,
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 12.0),
+      child: TextFormField(
+        validator: (val) {
+          return validator != null ? validator!(val) : null;
+        },
+        autovalidateMode: AutovalidateMode.onUserInteraction,
+        controller: controller,
+        decoration: InputDecoration(
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(8.0),
+            borderSide: BorderSide(width: 0, style: BorderStyle.none),
           ),
-        ),
-        focusedErrorBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(8),
-          borderSide: BorderSide(
-            color: Colors.red,
-            width: 1.5,
+          hoverColor: Colors.white,
+          focusColor: Colors.white,
+          filled: true,
+          fillColor: Colors.white,
+          errorBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(8),
+            borderSide: BorderSide(
+              color: Colors.red,
+              width: 1.5,
+            ),
           ),
+          focusedErrorBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(8),
+            borderSide: BorderSide(
+              color: Colors.red,
+              width: 1.5,
+            ),
+          ),
+          alignLabelWithHint: true,
+          floatingLabelBehavior: FloatingLabelBehavior.never,
+          labelText: label,
+          labelStyle: GoogleFonts.ubuntu(
+            fontSize: 16.0,
+            letterSpacing: 1.1,
+            color: MColors.hintText
+          ),
+          prefixIcon: prefixIcon,
+          suffixIcon: suffixIcon,
         ),
-        alignLabelWithHint: true,
-        floatingLabelBehavior: FloatingLabelBehavior.never,
-        labelText: label,
-        labelStyle: GoogleFonts.ubuntu(
-          fontSize: 16.0,
-          letterSpacing: 1.1,
-          color: MColors.hintText
+        style: GoogleFonts.ubuntu(
+            fontSize: 16.0,
+            letterSpacing: 1.1,
         ),
-        prefixIcon: prefixIcon,
-        suffixIcon: suffixIcon,
+        onTap: onTap,
+        minLines: minLines,
+        maxLines: maxLines,
+        readOnly: enabled!,
       ),
-      style: GoogleFonts.ubuntu(
-          fontSize: 16.0,
-          letterSpacing: 1.1,
-      ),
-      onTap: onTap,
-      minLines: minLines,
-      maxLines: maxLines,
-      readOnly: enabled!,
     );
   }
 }
