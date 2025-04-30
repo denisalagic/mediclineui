265dbcd3caaef3c24654531c0c4a5ca78757d063
dalagic <denisalagichr@gmail.com>
2025-04-30 08:37:08 +0200

    mlabel

--- lib/mlabel/mlabel.dart
+++ lib/mlabel/mlabel.dart
@@ -2,18 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class MLabel extends StatelessWidget {
-  const MLabel({super.key, required this.label});
+  const MLabel({super.key, required this.label, this.isBig = false,});
 
   final String label;
+  final bool isBig;
 
   @override
   Widget build(BuildContext context) {
     return Text(
       label.toUpperCase(),
       style: GoogleFonts.ubuntu(
-        fontSize: 14.0,
+        fontSize: isBig ? 16.0 : 14.0,
         letterSpacing: 2.0,
-        color: Colors.grey.shade800,
+        color: isBig ? Colors.black87 : Colors.grey.shade800,
       ),
     );
   }
