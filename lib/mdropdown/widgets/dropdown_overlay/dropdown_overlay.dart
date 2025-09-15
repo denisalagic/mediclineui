8a2f2dfb7223e7cf36c3a34916108f494922416d
dalagic <denisalagichr@gmail.com>
2025-09-15 17:26:28 +0200

    textfield on changed

--- lib/mdropdown/widgets/dropdown_overlay/dropdown_overlay.dart
+++ lib/mdropdown/widgets/dropdown_overlay/dropdown_overlay.dart
@@ -162,6 +162,9 @@ class _DropdownOverlayState<T> extends State<_DropdownOverlay<T>> {
             result.toString(),
             maxLines: widget.maxLines,
             //overflow: TextOverflow.ellipsis,
+            maxLines: widget.maxLines,
+            softWrap: true,
+            overflow: TextOverflow.visible,
             style: widget.listItemStyle ?? GoogleFonts.ubuntu(
               fontSize: 16.0,
             ),
